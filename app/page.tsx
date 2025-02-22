19158f29afb38defd046bfcf7925acc0de91a155
sifXt <sifatsingh7059@gmail.com>
2025-02-22 16:38:06 +0530

    maked ui as prev

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import { Hotel_new_AI } from "./components/aiNew";
 
 export default function Home() {
-  return (
-    <div style={{ backgroundColor: "#f0f0f0", padding: "20px" }}>
-      <Hotel_new_AI></Hotel_new_AI>
-    </div>
-  );
+	return (
+		<div>
+			<Hotel_new_AI></Hotel_new_AI>
+		</div>
+	);
 }
